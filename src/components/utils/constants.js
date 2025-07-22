76773098f05bc22a04a0f3ff1e17e05e9e0af665
Manahil Sami <manahil_sami@outlook.com>
2025-07-22 14:43:25 -0400

    added images

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -3,35 +3,40 @@ export const SidebarData = [
     name: "Yosemite National Park",
     town: "Yosemite Valley",
     rating: 4.8,
-    image: "https://via.placeholder.com/300x200?text=Yosemite",
+    image:
+      "https://www.hertz.com/content/dam/hertz/global/blog-articles/planning-a-trip/yosemite/Yosemite-National-Park-Header.jpg",
     location: { lat: 37.8651, lng: -119.5383 },
   },
   {
     name: "Golden Gate Bridge",
     town: "San Francisco",
     rating: 4.7,
-    image: "https://via.placeholder.com/300x200?text=Golden+Gate",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/960px-GoldenGateBridge-001.jpg",
     location: { lat: 37.8199, lng: -122.4783 },
   },
   {
     name: "Disneyland",
     town: "Anaheim",
     rating: 4.6,
-    image: "https://via.placeholder.com/300x200?text=Disneyland",
+    image:
+      "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1349/464/75/dam/disneyland/attractions/disneyland/sleeping-beauty-castle-walkthrough/sleeping-beauty-castle-exterior-16x9.jpg?1750708846734",
     location: { lat: 33.8121, lng: -117.919 },
   },
   {
     name: "Hollywood Sign",
     town: "Los Angeles",
     rating: 4.5,
-    image: "https://via.placeholder.com/300x200?text=Hollywood",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Hollywood_Sign_%28Zuschnitt%29.jpg/1200px-Hollywood_Sign_%28Zuschnitt%29.jpg",
     location: { lat: 34.1341, lng: -118.3215 },
   },
   {
     name: "Alcatraz Island",
     town: "San Francisco",
     rating: 4.4,
-    image: "https://via.placeholder.com/300x200?text=Alcatraz",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Alcatraz_2021.jpg/1200px-Alcatraz_2021.jpg",
     location: { lat: 37.827, lng: -122.423 },
   },
 ];
