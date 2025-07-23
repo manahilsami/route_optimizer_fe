8834ffce77afddc0fc0fb0c24a59bae34dcaaccb
Manahil Sami <manahil_sami@outlook.com>
2025-07-23 16:50:27 -0400

    added necessary code for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ghPages from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/route_optimizer_fe/",
+  plugins: [react(), ghPages()],
   server: {
     port: 3000,
   },
